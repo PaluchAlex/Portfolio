f4f5abf05bc546ddd955d79d7578796f80fbd7fb
DESKTOP-EFDVTUD\Alex <alexpaluch43@gmail.com>
2024-02-12 23:03:03 +0200

    setup

---
+++ src/utils.js
@@ -0,0 +1,4 @@
+export const getImageUrl = (path) => {
+    // makes it easyer to dinamycally import images into our different components
+    return new URL(`assets/${path}`, import.meta.url).href;
+};
