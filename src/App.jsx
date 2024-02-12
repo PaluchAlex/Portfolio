2bd96f5df5c7471ed980b11b578ecc41b7f9e303
DESKTOP-EFDVTUD\Alex <alexpaluch43@gmail.com>
2024-02-12 22:37:42 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import styles from './App.modules.css';
+
+function App() {
+  //const [count, setCount] = useState(0)
+
+  return (
+    <div className={styles.App}> 
+      Hello World
+    </div>
+  )
+}
+
+export default App
