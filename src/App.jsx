31c401ad8cf9d1cc2a683ff14b6f147e4e7c192b
DESKTOP-EFDVTUD\Alex <alexpaluch43@gmail.com>
2024-02-13 23:12:16 +0200

    add temporary footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import system from "./App.module.css";
 import { Hero } from "./components/Hero/Hero";
 import { Navbar } from "./components/Navbar/Navbar";
+import { About } from "./components/About/About";
+
 
 function App() {
     return (
         <div className={system.App}>
             <Navbar />
             <Hero />
+            <About />
+            <footer>Icons by <a href="https://icons8.com">Icons8.com</a> </footer>
         </div>
     );
 }
