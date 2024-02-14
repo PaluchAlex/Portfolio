bd37606340bc2a84825d6d845c28ba8984942927
DESKTOP-EFDVTUD\Alex <alexpaluch43@gmail.com>
2024-02-14 20:00:24 +0200

    add projects section

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import system from "./App.module.css";
+import styles from "./App.module.css";
 import { Hero } from "./components/Hero/Hero";
 import { Navbar } from "./components/Navbar/Navbar";
 import { About } from "./components/About/About";
 import { Contact } from "./components/Contact/Contact";
+import { Projects } from "./components/Projects/Projects";
 
 
 function App() {
     return (
-        <div className={system.App}>
+        <div className={styles.App}>
             <Navbar />
             <Hero />
             <About />
+            <Projects />
             <Contact />
         </div>
     );
